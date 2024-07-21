09e35c8aff92efbfd3a83860d14451cc03cb8579
SeungMin Baek <158240718+SeungMin-Baek98@users.noreply.github.com>
2024-07-21 17:22:49 +0900

    배포 할겁니데이~

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/CssStudy4/", // REPO_NAME을 실제 리포지토리 이름으로 변경
+  base: "/CssStudy3/", // REPO_NAME을 실제 리포지토리 이름으로 변경
 });
